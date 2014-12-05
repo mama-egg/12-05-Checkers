6b8e45371d72e84baa17e1ea41e2946ba83c6b18
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-05 16:46:06 -0700

    Update CheckersMove.java

--- src/CheckersMove.java
+++ src/CheckersMove.java
@@ -13,7 +13,7 @@ class CheckersMove {
 	private int toCol;
 	
 	// Constructor.  Just set the values of the instance variables.
-	for col (int r1, int c1, int r2, int c2) {
+	public CheckersMove(int r1, int c1, int r2, int c2) {
 		fromRow = r1;
 		fromCol = c1;
 		toRow = r2;
@@ -39,7 +39,6 @@ class CheckersMove {
 	}
 	
 	private boolean isJump() {
-		return false;
 		
 		private int temp1 = toRow - fromRow;
 		private int temp1 = toCol - fromCol;
@@ -49,6 +48,7 @@ class CheckersMove {
 				return true;
 			}
 		}
+		return false;
 	}
 	
 }   // end class CheckersMove.
